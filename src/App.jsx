fb52a351a7729fc33154e24b5e54d122bd35f6f2
Felipe <felipe21fab@gmail.com>
2024-05-16 15:09:39 -0300

    Created project and add axios, tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-red-600">Meu Projeto com Tailwind CSS</h1>
+    </>
+  )
+}
+
+export default App
