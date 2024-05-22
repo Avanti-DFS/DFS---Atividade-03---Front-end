f08a13b6d34f3c13f5117c2ad79decf59f6e2766
Felipe <felipe21fab@gmail.com>
2024-05-21 22:47:26 -0300

    Add logo and ingresso svg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import logo from "../assets/logo.svg"
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 bg-white-200 p-4">
-      <h1 className='w-full text-3xl font-bold'>Logo</h1>
+      <h1 className='w-full max-w-16'><img src={logo} alt="" /></h1>
       <ul className='flex'>
-        <li className='p-4'>Home</li>
-        <li className='p-4'>Eventos</li>
-        <li className='p-4'>Sobre</li>
+        <li className='p-4 font-medium'>Home</li>
+        <li className='p-4 font-medium'>Eventos</li>
+        <li className='p-4 font-medium'>Sobre</li>
         <li className='p-4'><a href="#" className='bg-[#f5ac3d] w-[200px] rounded-md font-medium my-6 mx-auto py-3 px-6'>Login</a></li>
       </ul>
     </nav>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-
+import imagemIngresso from "../assets/ingresso.svg"
 const Home = () => {
   return (
     <div className="min-h-screen bg-white-200 flex flex-col items-center justify-center">
       <div className="flex items-center justify-around w-full py-6">
-        <div>
-          Procurando por um evento?
+        <div className='text-7xl font-bold break-words max-w-[400px]'>
+          Procurando um evento
         </div>
-        <div>
-          Logo
+        <div className='text-9xl font-bold absolute left-32'>?</div>
+        <div className='max-w-[34rem]'> 
+        <img src={imagemIngresso} alt="" />
         </div>
       </div>
       <p className="text-center max-w-xl">
