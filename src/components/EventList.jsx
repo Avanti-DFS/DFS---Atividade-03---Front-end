5ab88bb2a33467a16b1d37bc2dd015a6e312c868
Felipe <felipe21fab@gmail.com>
2024-05-23 17:55:20 -0300

    Add searchbar

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -6,6 +6,7 @@ import EventCard from './EventCard';
 const EventList = () => {
     const [eventos, setEventos] = useState([]);
     const [eventosRender, setEventosRender] = useState([]);
+    const [search, setSearch] = useState("");
 
     async function getAllEventos() {
         try {
@@ -27,14 +28,44 @@ const EventList = () => {
         getAllEventos();
     }, [])
 
+    useEffect(() => {
+        const filtered = eventos.filter((evento) =>
+          Object.values(evento).some((value) =>
+            value.toString().toLowerCase().includes(search.toLowerCase())
+          )
+        );  
+        setEventosRender(filtered)
+      },[search])
+
+      const handleSearchChange = (e) => {
+        const value = e.target.value;
+        setSearch(value);
+        if (value === "") {
+          getEventos
+          setEventosRender(eventos)
+        }
+      };
+
     return (
-        <div className="grid grid-cols-2 gap-4">
-          {eventosRender.map(event => (
-            <div key={event.id} className="flex justify-center">
-              <EventCard name={event.nome} date={formatarData(event.data)} />
+        <>
+            <div className="mb-3">
+                <input
+                    type="text"
+                    className="border border-gray-300 rounded-md py-2 px-4 w-full"
+                    placeholder="Search..."
+                    value={search}
+                    onChange={handleSearchChange}
+                />
             </div>
-          ))}
-        </div>
+            <div className="grid grid-cols-2 gap-4">
+            {eventosRender.map(event => (
+                <div key={event.id} className="flex justify-center">
+                <EventCard name={event.nome} date={formatarData(event.data)} />
+                </div>
+            ))}
+            </div>
+        </>
+        
       );
       
     }
