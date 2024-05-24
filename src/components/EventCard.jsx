1b24324de44ae13d3a3fd1d4053b04b25cd84e83
Felipe <felipe21fab@gmail.com>
2024-05-24 00:01:57 -0300

    Fix EventList layout when odd event number

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const EventCard = ({ name, date }) => {
   return (
-    <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+    <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 min-w-[576px] min-h-[174px]">
       <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="src\assets\nainoa-shizuru-NcdG9mK3PBY-unsplash.jpg" alt="" />
       <div className="flex flex-col justify-between p-4 leading-normal">
         <h5 className="text-start mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
